5a73971eb01ea4af29df8c96fcec2d783c2572a2
Jose Antonio Pardo <joseantonio.pardoq@gmail.com>
2018-04-02 15:26:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+  row1 = "  #{board[0]  |    |    "
+  row2 = 
+  
+  line = "-----------"
